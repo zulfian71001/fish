149882f2d3c9582a6cdbe05d89411e1eef01d381
zulfian71001 <zulfian71001@gmail.com>
2024-06-20 21:06:08 +0700

    new address

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ["res.cloudinary.com"],
+    domains: ["res.cloudinary.com", "example.com"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+      },
+    ]
   },
 };
