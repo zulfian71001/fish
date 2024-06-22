18b0cdf840e03790826b2091f50a7d8aed55f444
zulfian71001 <zulfian71001@gmail.com>
2024-06-22 13:11:37 +0700

    authentication19

--- src/GlobalRedux/features/authReducer.ts
+++ src/GlobalRedux/features/authReducer.ts
@@ -130,7 +130,7 @@ export const user_info = createAsyncThunk(
   "auth/user_info",
   async (_, { rejectWithValue, fulfillWithValue }) => {
     try {
-      const { data } = await api.post(`/get-user`, {
+      const { data } = await api.get(`/get-user`, {
         withCredentials: true,
       });
       return fulfillWithValue(data);
