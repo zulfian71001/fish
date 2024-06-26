eb9b955865b856ff4f0fe760efdace93e3d1d2a5
zulfian71001 <zulfian71001@gmail.com>
2024-06-26 17:12:27 +0700

    authentication37

--- src/components/front-office/Products.tsx
+++ src/components/front-office/Products.tsx
@@ -45,7 +45,7 @@ const Products = () => {
   };
   return (
     <div className="flex justify-center items-center">
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 my-10">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 my-10">
         {products &&
           products?.map((data: any, i: number) => (
             <Product
